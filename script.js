8e9c69a10781bb634a471ce28c72e0e0fe699c32
BagirovJef <bagirovjef1@icloud.com>
2026-03-07 00:47:27 +0300

    Initial commit: QA Learning project

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.getElementById('app');
+    
+    app.innerHTML = `
+        <p>Добро пожаловать в QA Learning!</p>
+        <p>Здесь вы изучите тестирование программного обеспечения.</p>
+    `;
+    
+    console.log('QA Learning приложение запущено');
+});
